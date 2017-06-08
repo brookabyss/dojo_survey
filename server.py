229ccdc3131d4e49fa3ac4d318cdd0ae1e64a893
Brook Kebede <brookabyss@gmail.com>
2017-06-08 11:18:36 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask ,render_template,redirect,session,request
+app=Flask(__name__)
+app.secret_key="12345"
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process' ,methods=['POST'])
+def create_user():
+    session["name"]=request.form["name"]
+    session["location"]=request.form["location"]
+    session["language"]=request.form["language"]
+    session["location"]=request.form["location"]
+    print session["name"]
+    return redirect('/result')
+
+@app.route('/result')
+def show():
+    return render_template('show.html')
+
+app.run(debug=True)
